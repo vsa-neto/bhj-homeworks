e4b64f65db2719b0f925e6525034cb0616a3c1f9
SerVin <v.s.work@yandex.ru>
2025-10-26 19:13:41 +0300

    task--js-features completed_2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,17 +1,17 @@
+"use strict";
 
-
-const clicker = document.getElementById("cookie");
+const cookie = document.getElementById("cookie");
 const counter = document.getElementById("clicker__counter");
 const speedometr = document.getElementById("clicker__speed");
 let count = 0;
 let speed = 0;
 
-clicker.onclick = () => {
+cookie.onclick = () => {
     if (count > 0) {
         speedometr.textContent = ((Date.now() - speed) / 1000).toFixed(1);
     }
     count++;
-    clicker.width = 180;
+    cookie.width = 180;
     setTimeout(() => {
         clicker.width = 200;
     }, 100);
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
